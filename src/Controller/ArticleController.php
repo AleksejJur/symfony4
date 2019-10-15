ce91b6cbbafa3539df18e735823674caacebda2e
Aleksej Jurkoit <aleksej.jurkoit@nfq.lt>
2019-10-15 17:38:11 +0300

    Services

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,9 +2,9 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\{JsonResponse as JsonResponse, Response as Response};
-//use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
@@ -37,9 +37,11 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
      */
-    public function toogleArticleHeart($slug)
+    public function toogleArticleHeart($slug, LoggerInterface $logger)
     {
         // TODO - actually heart/unheart the article!
+
+        $logger->info('Article is being hearted!');
         return new JsonResponse(['hearts' => rand(5, 100)]);
 
 //        return $this->json(['hearts' => rand(5, 100)]);
