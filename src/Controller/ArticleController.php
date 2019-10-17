8b61e903f343041dc3c6e52d5868cc58fa722e1c
Aleksej Jurkoit <aleksej.jurkoit@nfq.lt>
2019-10-17 11:00:18 +0300

    Services config (MarkdownBUndle)

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use Michelf\MarkdownInterface;
+use Psr\Cache\InvalidArgumentException;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\{JsonResponse as JsonResponse, Response as Response};
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
@@ -20,8 +22,16 @@ class ArticleController extends AbstractController
 
     /**
      * @Route("/news/{slug}", name="article_show")
+     *
+     * @param $slug
+     * @param MarkdownInterface $markdown
+     * @param AdapterInterface $cache
+     *
+     * @throws InvalidArgumentException
+     *
+     * @return Response
      */
-    public function show($slug, MarkdownInterface $markdown)
+    public function show($slug, MarkdownInterface $markdown, AdapterInterface $cache): Response
     {
         $comments = [
             'I ate a normal rock once. It did NOT taste like bacon!',
@@ -30,25 +40,34 @@ class ArticleController extends AbstractController
         ];
 
         $articleContent = <<<EOF
-        Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
-        lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
-        labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
-        turkey shank eu pork belly meatball non cupim.
-        
-        Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur
-        laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder,
-        capicola biltong frankfurter boudin cupim officia. Exercitation fugiat consectetur ham. Adipisicing
-        picanha shank et filet mignon pork belly ut ullamco. Irure velit turducken ground round doner incididunt
-        occaecat lorem meatball prosciutto quis strip steak.
-        
-        Meatball adipisicing ribeye bacon strip steak eu. Consectetur ham hock pork hamburger enim strip steak
-        mollit quis officia meatloaf tri-tip swine. Cow ut reprehenderit, buffalo incididunt in filet mignon
-        strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lorem shoulder tail consectetur
-        cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck
-        fugiat.
+Spicy **jalapeno** bacon ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
+lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
+labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
+**turkey** shank eu pork belly meatball non cupim.
+
+Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur
+laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder,
+capicola biltong frankfurter boudin cupim officia. Exercitation fugiat consectetur ham. Adipisicing
+picanha shank et filet mignon pork belly ut ullamco. Irure velit turducken ground round doner incididunt
+occaecat lorem meatball prosciutto quis strip steak.
+
+Meatball adipisicing ribeye bacon strip steak eu. Consectetur ham hock pork hamburger enim strip steak
+mollit quis officia meatloaf tri-tip swine. Cow ut reprehenderit, buffalo incididunt in filet mignon
+strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lorem shoulder tail consectetur
+cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck
+fugiat.
 EOF;
 
         $articleContent = $markdown->transform($articleContent);
+        dump($markdown);die;
+        $item = $cache->getItem('markdown_' . md5($articleContent));
+
+        if (!$item->isHit()) {
+            $item->set($markdown->transform($articleContent));
+            $cache->save($item);
+        }
+
+        $articleContent = $item->get();
 
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
@@ -60,8 +79,12 @@ EOF;
 
     /**
      * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     *
+     * @param $slug
+     * @param LoggerInterface $logger
+     * @return JsonResponse
      */
-    public function toogleArticleHeart($slug, LoggerInterface $logger)
+    public function toogleArticleHeart($slug, LoggerInterface $logger): JsonResponse
     {
         // TODO - actually heart/unheart the article!
 
